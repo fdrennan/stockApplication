2d0c668d8c318caf48ca9426c659ff53115012c6
Freddy Drennan <drennanfreddy@gmail.com>
2020-01-28 12:56:08 -0700

    update

---
+++ stockAPI/misc/README.md
@@ -0,0 +1,2 @@
+docker build -t reactr .
+docker build -t myname/apache-test .
---
+++ stockAPI/misc/plumber.R
@@ -0,0 +1,33 @@
+#* @Plumber Example
+
+library(plumber)
+library(stockAPI)
+
+#* @filter cors
+cors <- function(req, res) {
+
+  res$setHeader("Access-Control-Allow-Origin", "*")
+
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods","*")
+    res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200
+    return(list())
+  } else {
+    plumber::forward()
+  }
+
+}
+
+#* @jpeg (width = 1000, height = 800)
+#* @param stocks  Stocks in JSON
+#* @get /get_stocks
+get_stocks <- function(stocks = '["AAPL"]') {
+  stockAPI::get_stocks(stocks = stocks, DATA = FALSE)
+}
+
+#* @param stocks  Stocks in JSON
+#* @get /get_stocks_data
+get_stocks_data <- function(stocks = '["AAPL"]') {
+  stockAPI::get_stocks(stocks = stocks, DATA = TRUE)
+}
